38520f6d837d48018f3e1d6037b05d1e32def8fd
Tommi Lehtisaari <tommi.lehtisaari@fraktio.fi>
2020-12-01 23:10:47 +0200

    Initial Commit

---
+++ wonders-of-nature/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wonders-of-nature
+//
+//  Created by Tommi Lehtisaari on 1.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
